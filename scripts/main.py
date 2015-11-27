8f08244e61b19f9b9b82e534d865c6f3d91d6226
Krzysztof Langner <klangner.mbank@gmail.com>
2015-11-27 15:12:26 +0100

    Create solution with deep learning

--- scripts/main.py
+++ scripts/main.py
@@ -27,37 +27,54 @@ train = cv_train.drop('label', 1) / 255.0
 test_labels = pd.get_dummies(cv_test['label'])
 test = cv_test.drop('label', 1) / 255.0
 
-# Define graph of computation
+
 # Placeholder for input data
-x = tf.placeholder("float", [None, 784])
-# Linear regression coefficients
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-# Model
-y = tf.nn.softmax(tf.matmul(x,W) + b)
-# Place holder for correct values
-y_ = tf.placeholder("float", [None,10])
-# Cost function (Cross entropy)
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-# Minimize cost function using Gradient Descent (single step), with learning rate = 0.01
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-# Init all variables
-init = tf.initialize_all_variables()
-# Create session and run variable initialization
-sess = tf.Session()
-sess.run(init)
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
 
-# Do 1000 steps of training
-print("Start learning")
-for i in range(1000):
-    if (i+1) % 100 == 0:
-        print(i+1)
-    (A, B, label_a, label_b) = cross_validation.train_test_split(train, train_labels, train_size=100)
-    sess.run(train_step, feed_dict={x: A, y_: label_a})
+# First layer 32 features with 5x5 patch (kernel)
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+# Reshape x as tensor
+x_image = tf.reshape(x, [-1,28,28,1])
+# We then convolve x_image with the weight tensor, add the bias, apply the ReLU function, and finally max pool.
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-# Get prediction and expected value
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-# Define accuracy operation
+# Second layer
+# The second layer will have 64 features for each 5x5 patch.
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# We reshape the tensor from the pooling layer into a batch of vectors, multiply by a weight matrix, add a bias, and apply a ReLU.
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# To reduce overfitting, we will apply dropout before the readout layer
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# Finally, we add a softmax layer, just like for the one layer softmax regression above.
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# Train and evaluate model
+cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-# check accuracy
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    (A, B, label_a, label_b) = cross_validation.train_test_split(train, train_labels, train_size=100)
+    if i % 100 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={x:A, y_: label_a, keep_prob: 1.0})
+        print "step %d, training accuracy %g"%(i, train_accuracy)
+    sess.run(train_step, feed_dict={x: A, y_: label_a, keep_prob: 0.5})
+
 print(sess.run(accuracy, feed_dict={x: test, y_: test_labels}))
